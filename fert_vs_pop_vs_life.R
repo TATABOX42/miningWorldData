b7eb1213e0254941f2c756139ac3788aaf3e598b
Benjamin Tovar <benjamin.tovarcis@gmail.com>
2015-03-17 13:32:44 -0600

    Fixed spacing

--- fert_vs_pop_vs_life.R
+++ fert_vs_pop_vs_life.R
@@ -72,34 +72,34 @@ population_m$variable <- as.numeric(gsub("X","",population_m$variable))
 library(ggplot2)
 library(gridExtra)
 
-	ggplot(fertility_m) +
-		aes(x=as.factor(variable),y=value) + 
-		geom_boxplot(aes(colour=as.factor(variable),fill=as.factor(variable)),alpha=0.6) + 
-		theme(legend.position="none",axis.text.x = element_text(angle=90, vjust=1)) + 
-		labs(title="Fertility per year: all countries",x="Year",y="fertility")
+  ggplot(fertility_m) +
+    aes(x=as.factor(variable),y=value) + 
+    geom_boxplot(aes(colour=as.factor(variable),fill=as.factor(variable)),alpha=0.6) + 
+    theme(legend.position="none",axis.text.x = element_text(angle=90, vjust=1)) + 
+    labs(title="Fertility per year: all countries",x="Year",y="fertility")
 
-	ggplot(life_expectancy_m) +
-		aes(x=as.factor(variable),y=value) + 
-		geom_boxplot(aes(colour=as.factor(variable),fill=as.factor(variable)),alpha=0.6) + 
-		theme(legend.position="none",axis.text.x = element_text(angle=90, vjust=1)) + 
-		labs(title="Life expectancy per year: all countries",x="Year",y="Life expectancy")
+  ggplot(life_expectancy_m) +
+    aes(x=as.factor(variable),y=value) + 
+    geom_boxplot(aes(colour=as.factor(variable),fill=as.factor(variable)),alpha=0.6) + 
+    theme(legend.position="none",axis.text.x = element_text(angle=90, vjust=1)) + 
+    labs(title="Life expectancy per year: all countries",x="Year",y="Life expectancy")
 
 
 # **********************
 # values in all regions
 # **********************
 
-	ggplot(fertility_m) +
-		aes(x=Region_ID,y=value) + 
-		geom_boxplot(aes(colour=Region,fill=Region),alpha=0.6) + 
-		theme(legend.position="right",axis.text.x = element_text(angle=90, vjust=1)) + 
-		labs(title="Fertility per region: all years",x="Region",y="Fertility")
+  ggplot(fertility_m) +
+    aes(x=Region_ID,y=value) + 
+    geom_boxplot(aes(colour=Region,fill=Region),alpha=0.6) + 
+    theme(legend.position="right",axis.text.x = element_text(angle=90, vjust=1)) + 
+    labs(title="Fertility per region: all years",x="Region",y="Fertility")
 
-	ggplot(life_expectancy_m) +
-		aes(x=Region_ID,y=value) + 
-		geom_boxplot(aes(colour=Region,fill=Region),alpha=0.6) + 
-		theme(legend.position="right",axis.text.x = element_text(angle=90, vjust=1)) + 
-		labs(title="Life expectancy per region: all years",x="Region",y="Life expectancy")
+  ggplot(life_expectancy_m) +
+    aes(x=Region_ID,y=value) + 
+    geom_boxplot(aes(colour=Region,fill=Region),alpha=0.6) + 
+    theme(legend.position="right",axis.text.x = element_text(angle=90, vjust=1)) + 
+    labs(title="Life expectancy per region: all years",x="Region",y="Life expectancy")
 
 # **********************
 # regressions and correlations 
@@ -111,9 +111,9 @@ library(gridExtra)
 # extract shared data shared by the 3 databases
 years <- paste("X",1961:2012,sep="")
 # set the country names shared in the 3 databases
-countries_all <-	c(as.character(fertility$Country_Code),
-							as.character(life_expectancy$Country_Code),
-							as.character(population$Country_Code))
+countries_all <-  c(as.character(fertility$Country_Code),
+              as.character(life_expectancy$Country_Code),
+              as.character(population$Country_Code))
 countries_table <- table(countries_all)
 index <- countries_table == 3
 countries_unique <- names(countries_table)[index]
@@ -123,8 +123,8 @@ countries_unique <- names(countries_table)[index]
 
 # extract subdatabases
 M <- list(fertility=fertility[countries_unique,years],
-					life_expectancy=life_expectancy[countries_unique,years],
-					population_norm=log10(population[countries_unique,years]))
+          life_expectancy=life_expectancy[countries_unique,years],
+          population_norm=log10(population[countries_unique,years]))
 
 # extract mean per country and per year
 fertility_mean_per_country <- rowMeans(M$fertility)
@@ -132,16 +132,16 @@ life_expectancy_mean_per_country <- rowMeans(M$life_expectancy)
 population_mean_per_country <- rowMeans(M$population)
 
 mean_pear_country <- data.frame(fertility=fertility_mean_per_country,
-														life_expectancy=life_expectancy_mean_per_country,
-														population=population_mean_per_country)
+                            life_expectancy=life_expectancy_mean_per_country,
+                            population=population_mean_per_country)
 
 
 # load library
 library("GGally")
 
-	ggpairs(mean_pear_country, title = "Pairwise comparison in all regions",
-		lower=list(continuous="smooth",params=c(colour="#009688",alpha=0.7)),
-	  diag=list(continuous="density",params=c(colour="red",alpha=1)), 
-	  upper=list(params=list(size=10)), 
-	  axisLabels='show')
+  ggpairs(mean_pear_country, title = "Pairwise comparison in all regions",
+    lower=list(continuous="smooth",params=c(colour="#009688",alpha=0.7)),
+    diag=list(continuous="density",params=c(colour="red",alpha=1)), 
+    upper=list(params=list(size=10)), 
+    axisLabels='show')
 
